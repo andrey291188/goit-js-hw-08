a1a91b459386609b745eb9c984363008dec0ebdb
Andrey <andrey291188@gmail.com>
2023-06-23 16:45:51 +0300

    full homework

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,42 @@
+import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+
+const TIME_SAVED = "videoplayer-current-time";
+const timeNow = JSON.parse(localStorage.getItem(TIME_SAVED)) ?? [];
+
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 640
+});
+
+function setTime ({ seconds }) {
+   
+    const timePlayer = seconds;
+    if (!timeNow.length) {
+        timeNow.push(timePlayer);
+    } else {
+        timeNow.pop()
+        timeNow.push(timePlayer)
+    }
+     return localStorage.setItem(TIME_SAVED, JSON.stringify(timeNow))   
+}
+
+player.on('timeupdate', throttle(setTime, 1000));
+
+
+player.setCurrentTime(timeNow).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+
+        default:
+
+            break;
+    }
+});
+
+
+
+  
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from "lodash.throttle";
+
+const inputForm = document.querySelector(".feedback-form");
+const inputSubmit = document.querySelector(".feedback-form");
+
+const STORY_KEY = "feedback-form-state";
+const dataForm = {};
+const messagSave = JSON.parse(localStorage.getItem(STORY_KEY));
+
+inputForm.addEventListener("input", throttle(createKey, 500));
+inputSubmit.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit (evt) {
+    evt.preventDefault();
+    evt.target.reset();
+    localStorage.removeItem(STORY_KEY)
+}
+
+function createKey (evt) {
+    dataForm[evt.target.name] = evt.target.value; 
+    localStorage.setItem(STORY_KEY, JSON.stringify(dataForm));
+
+}
+
+function populateTextArea () {
+
+    if (messagSave) {
+        inputForm.email.value = messagSave.email;
+        inputForm.message.value = messagSave.message;
+    }
+}
+populateTextArea(messagSave)
+
+
 
